56f14b4715001c69e2701285cb79974a2b9934eb
rakibsarowar <rakibsarowar@gmail.com>
2023-04-25 12:03:33 +0600

    working on context router

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,9 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProviders';
 
 const Home = () => {
-    const user = useContext(AuthContext)
+const {user} = useContext(AuthContext);
+    console.log(user);
+
     return (
         <div>
         <h2>This is home  {user && <span>{user.displayName}</span>}</h2>
--- src/components/providers/AuthProviders.jsx
+++ src/components/providers/AuthProviders.jsx
@@ -1,14 +1,29 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { getAuth } from "firebase/auth";
+import app from '../../firebase/firebase.config';
 
-// Step 01  create UserContext/-------------
+// Step 01  create UserContext & Export it-------------
 export const AuthContext = createContext(null)
+
+// Step 03 ---------------firebase----------------
+const auth = getAuth(app)
 
 
 const AuthProviders = ({children}) => {
+// Step 02: create a user & return authContext Provider with value-----------------------------------
 
-    const user = {displayName: 'Sagor'}
+// Step 04 - share data in state --------------------
+const [user, setUser] = useState(null);
+
+const authInfo = {
+    user,
+}
+
+
+
+
     return (
-        <AuthContext.Provider value={user}>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
